f9d50a54bb8e10035a676e83a0d9e2044e11f924
skreimeyer <samuel.kreimeyer@gmail.com>
2019-02-11 21:39:33 -0600

    Lots of cleanup. Typical sections added.

--- src/structural/beams/mod.rs
+++ src/structural/beams/mod.rs
@@ -21,6 +21,9 @@
 //! - D = deflection
 //!
 
+const PI:f32 = std::f32::consts::PI;
+
+
 /// This trait gives us a common interface to the formulas used for
 /// determining the properties of beams which vary with a particular beam
 /// cross section.
@@ -31,48 +34,161 @@ pub trait Beam {
     fn radius_of_gyration(&self) -> f32  {
         (self.moment_of_inertia() / self.area()).powf(0.5)
     }
+    // More to come . . .
 }
 
-/// A beam with a solid, rectangular cross section
-/// d = major axis length
-/// b = minor axis length
 #[derive(Debug)]
-pub struct RectangularBeam {
-    pub d: f32,
-    pub b: f32,
+pub struct PolygonalBeam {
+    pub R: f32, // Circumscribed radius
+    pub r: f32, // Inscribed radius (apothem)
+    pub n: i32, // Number of sides
+    pub s: f32, // Side length
 }
 
-/// Gere, J. M. and Timnko, S., 1997, Mechanics of Materials 4th Ed., PWS Publishing Co.
-impl Beam for RectangularBeam {
+impl PolygonalBeam {
+    fn new(s: f32, n: i32) -> PolygonalBeam {
+        PolygonalBeam {
+            R: s / 2.0 / (180.0 / n as f32).sin(),
+            r: s / 2.0 /(180.0 / n as f32).tan(),
+            n: n,
+            s: s,
+        }
+    }
+}
+
+impl Beam for PolygonalBeam {
     fn area(&self) -> f32 {
-        self.d * self.b
+        self.n as f32 * self.s * self.r / 2.0
     }
     fn moment_of_inertia(&self) -> f32 {
-        self.b * self.d.powf(3.0) / 12.0
+        self.area() / 24.0 * (6.0 * self.R.powi(2) - self.s.powi(2))
     }
     fn section_modulus(&self) -> f32 {
-        self.b * self.d.powf(2.0) / 6.0
+        self.moment_of_inertia() / self.r
     }
 }
 
-/// A beam with a solid, triangular cross section
-/// d = major axis length
-/// b = minor axis length
 #[derive(Debug)]
-pub struct TriangularBeam {
-    pub d: f32,
-    pub b: f32,
+struct TrapezoidalBeam {
+    pub minor: f32,
+    pub major: f32,
+    pub height: f32,
+    diff_lengths : f32,
 }
 
-/// Gere, J. M. and Timnko, S., 1997, Mechanics of Materials 4th Ed., PWS Publishing Co.
-impl Beam for TriangularBeam {
+impl TrapezoidalBeam {
+    fn new(minor:f32,major:f32,height:f32) -> TrapezoidalBeam {
+        TrapezoidalBeam {
+            minor: minor,
+            major: major,
+            height: height,
+            diff_lengths: major - minor,
+        }
+    }
+}
+
+impl Beam for TrapezoidalBeam {
     fn area(&self) -> f32 {
-        self.d * self.b / 2.0
+        (self.minor + self.major) / 2.0 * self.height
     }
     fn moment_of_inertia(&self) -> f32 {
-        self.b * self.d.powf(3.0) / 36.0
+        (6.0 * self.minor.powi(2) + 6.0 * self.minor * self.diff_lengths + self.diff_lengths.powi(2))
+        / (36.0 * (2.0 * self.minor + self.diff_lengths)) * self.height.powi(3)
     }
     fn section_modulus(&self) -> f32 {
-        self.b * self.d.powf(2.0) / 24.0
+        (6.0 * self.minor.powi(2) + 6.0 * self.minor * self.diff_lengths + self.diff_lengths.powi(2))
+        / (12.0 * (3.0 * self.minor + 2.0 * self.diff_lengths)) * self.height.powi(2)
+    }
+}
+
+/// Gere, James M., "Mechanics of Materials," 6th Ed.
+#[derive(Debug)]
+struct IBeam {
+    width: f32,
+    height: f32,
+    flange: f32, // thickness
+    web: f32, // thickness
+    web_height: f32,
+}
+
+impl IBeam {
+    fn new(width:f32, height:f32, flange:f32, web:f32) -> IBeam {
+        IBeam {
+            width: width,
+            height: height,
+            flange: flange,
+            web: web,
+            web_height: height - 2.0 * flange,
+        }
+    }
+}
+
+impl Beam for IBeam {
+    fn area(&self) -> f32 {
+        2.0 * self.width * self.flange + self.web * self.web_height
+    }
+    fn moment_of_inertia(&self) -> f32 {
+        (self.width * self.height.powi(3) - self.width * self.web_height.powi(3)
+    + self.web * self.web_height.powi(3)) / 12.0
+    }
+    fn section_modulus(&self) -> f32 {
+        (self.width * self.height.powi(2) - self.web_height.powi(3) / self.height
+    * (self.width - self.web)) / 6.0
+    }
+}
+
+/// Gere, James M., "Mechanics of Materials," 6th Ed.
+#[derive(Debug)]
+struct CircularBeam {
+    rad: f32,
+    dia: f32,
+}
+
+impl CircularBeam {
+    fn new(rad:f32) -> CircularBeam {
+        CircularBeam {
+            rad: rad,
+            dia: 2.0 * rad
+        }
+    }
+}
+
+impl Beam for CircularBeam {
+    fn area(&self) -> f32 {
+        PI*self.rad.powi(2)
+    }
+    fn moment_of_inertia(&self) -> f32 {
+        self.area() / 4.0 * self.rad.powi(2)
+    }
+    fn section_modulus(&self) -> f32 {
+        self.moment_of_inertia() / self.rad
+    }
+}
+
+/// Gere, James M., "Mechanics of Materials," 6th Ed.
+#[derive(Debug)]
+struct CircularTube {
+    inner_radius: f32,
+    outer_radius: f32,
+}
+
+impl CircularTube {
+    fn new(inner_radius:f32,outer_radius:f32) -> CircularTube{
+        CircularTube {
+            inner_radius: inner_radius,
+            outer_radius: outer_radius,
+        }
+    }
+}
+
+impl Beam for CircularTube {
+    fn area(&self) -> f32 {
+        PI * (self.outer_radius.powi(2) - self.inner_radius.powi(2))
+    }
+    fn moment_of_inertia(&self) -> f32 {
+        PI / 4.0 * (self.outer_radius.powi(4) - self.inner_radius.powi(4))
+    }
+    fn section_modulus(&self) -> f32 {
+        PI / 4.0 * (self.outer_radius.powi(4) - self.inner_radius.powi(4)) / self.outer_radius
     }
 }
